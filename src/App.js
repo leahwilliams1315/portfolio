fb70a4770867ba3905ceb6be97dba5f6777c91a8
leahwilliams1315 <leah1315@gmail.com>
2016-12-28 14:03:15 -0500

    toolbar was added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Leah's Portfolio</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <div className="App">
+                <AppBar
+                    title="Leah's Portfolio"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                />
+                <TextField
+                    hintText="Hint Text"
+                /><br />
+                <br />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
 import App from './App';
 import './index.css';
 
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+    palette: {
+        textColor: cyan500,
+    },
+});
+
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root')
 );
